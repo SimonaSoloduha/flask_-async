683574f04c77a45d15be581412fbdb0500b893cf
Soloduha Simona <ls460simona@gmail.com>
2021-07-31 13:41:14 +0300

    add timeout

--- app.py
+++ app.py
@@ -1,42 +1,50 @@
 import asyncio
 import json
+from datetime import timedelta
 
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SERVER_NAME'] = 'localhost:5000'
-
-ALL_RESULT = []
-TIME_LIMIT = 0.1
-
-
-async def async_get_data(file_name):
-    data = get_from_file(file_name)
-    ALL_RESULT.extend(data)
-    await asyncio.sleep(TIME_LIMIT)
-    return ALL_RESULT
+app.permanent_session_lifetime = timedelta(seconds=1)
+TIME_LIMIT = 3.0001
 
 
-def get_from_file(file_name):
-    with open(file_name, "r") as read_file:
-        data = json.load(read_file)
-        return data
+async def async_get_data(all_result):
+    task1 = asyncio.create_task(get_from_file('uploads/file1.json', all_result))
+    task2 = asyncio.create_task(get_from_file('uploads/file2.json', all_result))
+    task3 = asyncio.create_task(get_from_file('uploads/file3.json', all_result))
+
+    await task1
+    await task2
+    await task3
+
+    all_result.sort(key=lambda item: int(item.get("id")))
+
+    return all_result
+
+
+async def get_from_file(file_name, all_result):
+    try:
+        with open(file_name, "r") as read_file:
+            data = json.load(read_file)
+        all_result.extend(data)
+        await asyncio.sleep(TIME_LIMIT)
+        return all_result
+    except FileNotFoundError:
+        return []
 
 
 @app.route("/")
 async def get_data():
+    all_result = []
 
-        task1 = asyncio.create_task(async_get_data('uploads/file1.json'))
-        task2 = asyncio.create_task(async_get_data('uploads/file2.json'))
-        task3 = asyncio.create_task(async_get_data('uploads/file3.json'))
+    try:
+        await asyncio.wait_for(async_get_data(all_result), timeout=2)
+        return render_template('index.html', items=all_result)
 
-        await task1
-        await task2
-        await task3
-
-        ALL_RESULT.sort(key=lambda item: int(item.get("id")))
-        return json.dumps(ALL_RESULT)
+    except asyncio.TimeoutError:
+        return 'Ошибка! Timeout!'
 
 
 if __name__ == '__main__':
